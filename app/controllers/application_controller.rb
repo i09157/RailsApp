255f68cbbb868fbe905a3f262ab63d187588f4f1
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-11 14:27:05 +0900

    ログイン済みかを確認する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :authenticate_account! #認証済みかを確認
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+ before_action :authenticate_account!,only: :authentication
   def index
   end
 
