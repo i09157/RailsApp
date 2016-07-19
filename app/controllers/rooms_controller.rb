9be9d05406a536bf562f99cfa191cefe5c1049b9
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-20 00:52:27 +0900

    じゃんけんの判定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,39 @@
 class RoomsController < ApplicationController
   def show
     @messages = Message.all
+    @lists = Message.where(created_at: 1.minute.ago..Time.current)
+
+    if @lists.count == 2 then #じゃんけん判定
+       first,second = @lists
+       flash.now[:first] = "first:#{first.content}"
+       flash.now[:second] = "second:#{second.content}"
+
+       if first.content == "グー" then
+          if second.content == "チョキ" then
+            flash.now[:result] = "fisrt win"
+          elsif second.content == "パー" then
+            flash.now[:result] = "second win"
+          else
+            flash.now[:result] = "draw"
+          end
+       elsif fisrt.content == "チョキ" then
+          if second.content == "パー" then
+            flash.now[:result] = "first win"
+          elsif second.content == "グー" then
+            flash.now[:result] = "second win"
+          else
+            flash.now[:result] = "draw"
+          end
+       else
+         if second.content == "グー" then
+           flash.now[:result] = "first win"
+         elsif second.content == "チョキ" then
+           flash.now[:result] = "second win"
+         else
+           flash.now[:result] = "draw"
+         end
+       end
+
+    end
   end
 end
