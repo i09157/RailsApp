4e6b9647addea1368ef32c759193cd112df33ca7
kisukeeee <kisue4722@gmail.com>
2016-07-20 13:53:55 +0900

    connected database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 # before_action :authenticate_account!,only: :authentication 
   def index
     @user = User.find_by(:id => current_account.id)
+    if @user == nil
+        @user = User.new(id: current_account.id, name:current_account.email,win:0.0, lose:0.0, draw:0.0)
+        @user.save(:validate => false)
+    end
   end
 
   def authentication
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @user = User.all
     @account = Account.all
   end
 
@@ -27,17 +26,6 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1
