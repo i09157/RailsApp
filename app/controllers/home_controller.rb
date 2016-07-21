4f10c8871679fc0e91fa119ec2b42a4782bd229e
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-21 16:11:01 +0900

    勝敗引き分けの初期値設定（バグ修正）

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,9 @@ class HomeController < ApplicationController
  before_action :authenticate_account!,only: :authentication
   def index
      if account_signed_in?
-       if current_account.win == nil
-         current_account.update(win: 0.0)
-       elsif current_account.draw == nil
-         current_account.update(draw: 0.0)
-       elsif current_account.lose == nil
-         current_account.update(lose: 0.0)
-       end
+       current_account.update(win: 0.0) if current_account.win == nil
+       current_account.update(draw: 0.0) if current_account.draw == nil
+       current_account.update(lose: 0.0) if current_account.lose == nil
      end
     @accounts = Account.all
   end
