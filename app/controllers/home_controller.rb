e43c23696e9134cff3f2b6c031fa3a203a4196f7
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-21 18:07:56 +0900

    ランキングの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,17 @@ class HomeController < ApplicationController
        current_account.update(win: 0.0) if current_account.win == nil
        current_account.update(draw: 0.0) if current_account.draw == nil
        current_account.update(lose: 0.0) if current_account.lose == nil
+       current_account.update(rate: 0.0) if current_account.rate == nil
      end
-    @accounts = Account.all
-  end
+    # @accounts = Account.all
+    @search = Account.search(params[:q])
+    @accounts = @search.result
 
+    respond_to do |format|
+      format.html
+      format.json {render json: @accounts}
+    end
+  end
 
   def authentication
   end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,55 +13,58 @@ class RoomsController < ApplicationController
 
        if first.content == "グー" then
           if second.content == "チョキ" then
-            flash.now[:result] = first.email + ":win"
+            flash.now[:result] = first.email + ":win:" + first.content + "vs" + second.content
             @account1.update(win: @account1.win + 1.0)
             @account2.update(lose: @account2.lose + 1.0)
 
           elsif second.content == "パー" then
-            flash.now[:result] = second.email + ":win"
+            flash.now[:result] = second.email + ":win" + first.content + "vs" + second.content
             @account2.update(win: @account2.win + 1.0)
             @account1.update(lose: @account1.lose + 1.0)
 
           elsif second.content == "グー" then
-            flash.now[:result] = "draw"
+            flash.now[:result] = "draw" + first.content + "vs" + second.content
             @account1.update(draw: @account1.draw + 1.0)
             @account2.update(draw: @account2.draw + 1.0)
 
           end
        elsif first.content == "チョキ" then
           if second.content == "パー" then
-            flash.now[:result] = first.email + ":win"
+            flash.now[:result] = first.email + ":win" + first.content + "vs" + second.content
             @account1.update(win: @account1.win + 1.0)
             @account2.update(lose: @account2.lose + 1.0)
 
           elsif second.content == "グー" then
-            flash.now[:result] = second.email + ":win"
+            flash.now[:result] = second.email + ":win" + first.content + "vs" + second.content
             @account2.update(win: @account2.win + 1.0)
             @account1.update(lose: @account1.lose + 1.0)
 
           elsif second.content == "チョキ" then
-            flash.now[:result] = "draw"
+            flash.now[:result] = "draw" + first.content + "vs" + second.content
             @account1.update(draw: @account1.draw + 1.0)
             @account2.update(draw: @account2.draw + 1.0)
 
           end
        elsif first.content == "パー" then
          if second.content == "グー" then
-           flash.now[:result] = first.email + ":win"
+           flash.now[:result] = first.email + ":win" + first.content + "vs" + second.content
            @account1.update(win: @account1.win + 1.0)
            @account2.update(lose: @account2.lose + 1.0)
 
          elsif second.content == "チョキ" then
-           flash.now[:result] = second.email + ":win"
+           flash.now[:result] = second.email + ":win" + first.content + "vs" + second.content
            @account2.update(win: @account2.win + 1.0)
            @account1.update(lose: @account1.lose + 1.0)
 
           elsif second.content == "パー" then
-           flash.now[:result] = "draw"
+           flash.now[:result] = "draw" + first.content + "vs" + second.content
            @account1.update(draw: @account1.draw + 1.0)
            @account2.update(draw: @account2.draw + 1.0)
          end
        end
+
+       @account1.update(rate: (@account1.win/(@account1.win+ @account1.lose + @account1.draw)).round(3))
+       @account2.update(rate: (@account2.win/(@account2.win+ @account2.lose + @account2.draw)).round(3))
 
     end
   end
