540ddabb22a25479880ba07858e9f038ad50e116
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-22 01:22:01 +0900

    リアルタイムなじゃんけん判定

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,7 +9,52 @@ class RoomChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak(data)
-    Message.create! email: current_account.email ,content: data['message']
+
+  def speak(data) #クライアントサイドから送信されたメッセージを受信
+      if @first.nil? then
+        @first = data['message']
+        @first_email = current_account.email
+        ActionCable.server.broadcast 'room_channel', message: @first_email
+      else
+        case @first
+        when "グー" then
+          if data['message'] == "グー" then
+            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+            @first = nil
+          elsif data['message'] == "チョキ" then
+            ActionCable.server.broadcast 'room_channel', message: "先攻の勝ち"
+            @first = nil
+          elsif data['message'] == "パー" then
+            ActionCable.server.broadcast 'room_channel', message: "後攻の勝ち"
+            @first = nil
+          end
+        when "チョキ" then
+          if data['message'] == "グー" then
+            ActionCable.server.broadcast 'room_channel', message: "後攻の勝ち"
+            @first = nil
+          elsif data['message'] == "チョキ" then
+            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+            @first = nil
+          elsif data['message'] == "パー" then
+            ActionCable.server.broadcast 'room_channel', message: "先攻の勝ち"
+            @first = nil
+          end
+        when "パー" then
+          if data['message'] == "グー" then
+            ActionCable.server.broadcast 'room_channel', message: "先攻の勝ち"
+            @first = nil
+          elsif data['message'] == "チョキ" then
+            ActionCable.server.broadcast 'room_channel', message: "後攻の勝ち"
+            @first = nil
+          elsif data['message'] == "パー" then
+            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+            @first = nil
+          end
+        end
+      end
+      Message.create! email: current_account.email ,content: data['message']
+
   end
+
+
 end
