8b857c83f90f24601e3d920b7d4aacb000213921
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-21 15:47:20 +0900

    勝敗引き分けの初期値の設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
 class HomeController < ApplicationController
  before_action :authenticate_account!,only: :authentication
   def index
+     if account_signed_in?
+       if current_account.win == nil
+         current_account.update(win: 0.0)
+       elsif current_account.draw == nil
+         current_account.update(draw: 0.0)
+       elsif current_account.lose == nil
+         current_account.update(lose: 0.0)
+       end
+     end
+    @accounts = Account.all
   end
+
 
   def authentication
   end
