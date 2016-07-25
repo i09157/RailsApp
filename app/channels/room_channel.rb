db1c5c86ddb45770129fc029341d3a4550af2b17
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-07-25 17:54:10 +0900

    タイマー起動中のみじゃんけんの選択を受け付ける

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -4,6 +4,7 @@ class RoomChannel < ApplicationCable::Channel
     # current_account.appear
     stream_from "room_channel"
     stream_for current_account
+
   end
 
   def unsubscribed
@@ -26,74 +27,84 @@ class RoomChannel < ApplicationCable::Channel
   # end
 
   def speak(data) #クライアントサイドから送信されたメッセージを受信
+    if @flug == 1 then
+      Message.create! email: current_account.email ,content: data['message']
+      @lists = Message.where(created_at: 5  .second.ago..Time.now) #現時刻から二件
+      if @lists.count < 2 then
+        # ActionCable.server.broadcast 'room_channel', message: "あっ！勝負がしかけられました"
+      elsif @lists.count ==  2  then
+        first,second = @lists
+        @account1 =Account.find_by(email: first.email)
+        @account2 =Account.find_by(email: second.email)
+        if @account1 != @account2 then
+          case first.content
+          when "グー" then
+            if second.content == "グー" then
+              ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+              @account1.update(draw: @account1.draw + 1.0)
+              @account2.update(draw: @account2.draw + 1.0)
 
-    Message.create! email: current_account.email ,content: data['message']
-    @lists = Message.where(created_at: 5  .second.ago..Time.now) #現時刻から二件
-    if @lists.count < 2 then
-      # ActionCable.server.broadcast 'room_channel', message: "あっ！勝負がしかけられました"
-    elsif @lists.count ==  2  then
-      first,second = @lists
-      @account1 =Account.find_by(email: first.email)
-      @account2 =Account.find_by(email: second.email)
-      if @account1 != @account2 then
-        case first.content
-        when "グー" then
-          if second.content == "グー" then
-            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
-            @account1.update(draw: @account1.draw + 1.0)
-            @account2.update(draw: @account2.draw + 1.0)
+            elsif second.content == "チョキ" then
+              ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
+              @account1.update(win: @account1.win + 1.0)
+              @account2.update(lose: @account2.lose + 1.0)
+            elsif second.content == "パー" then
+              ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
+              @account2.update(win: @account2.win + 1.0)
+              @account1.update(lose: @account1.lose + 1.0)
+            end
+          when "チョキ" then
+            if second.content == "グー" then
+              ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
+              @account2.update(win: @account2.win + 1.0)
+              @account1.update(lose: @account1.lose + 1.0)
+            elsif second.content == "チョキ" then
+              ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+              @account1.update(draw: @account1.draw + 1.0)
+              @account2.update(draw: @account2.draw + 1.0)
+            elsif second.content == "パー" then
+              ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
+              @account1.update(win: @account1.win + 1.0)
+              @account2.update(lose: @account2.lose + 1.0)
+            end
+          when "パー" then
+            if second.content == "グー" then
+              ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
+              @account1.update(win: @account1.win + 1.0)
+              @account2.update(lose: @account2.lose + 1.0)
+            elsif second.content == "チョキ" then
+              ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
+              @account2.update(win: @account2.win + 1.0)
+              @account1.update(lose: @account1.lose + 1.0)
+            elsif second.content == "パー" then
+              ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
+              @account1.update(draw: @account1.draw + 1.0)
+              @account2.update(draw: @account2.draw + 1.0)
+            end
+          end
+          @account1.update(rate: (@account1.win/(@account1.win+ @account1.lose + @account1.draw)).round(3))
+          @account2.update(rate: (@account2.win/(@account2.win+ @account2.lose + @account2.draw)).round(3))
 
-          elsif second.content == "チョキ" then
-            ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
-            @account1.update(win: @account1.win + 1.0)
-            @account2.update(lose: @account2.lose + 1.0)
-          elsif second.content == "パー" then
-            ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
-            @account2.update(win: @account2.win + 1.0)
-            @account1.update(lose: @account1.lose + 1.0)
-          end
-        when "チョキ" then
-          if second.content == "グー" then
-            ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
-            @account2.update(win: @account2.win + 1.0)
-            @account1.update(lose: @account1.lose + 1.0)
-          elsif second.content == "チョキ" then
-            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
-            @account1.update(draw: @account1.draw + 1.0)
-            @account2.update(draw: @account2.draw + 1.0)
-          elsif second.content == "パー" then
-            ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
-            @account1.update(win: @account1.win + 1.0)
-            @account2.update(lose: @account2.lose + 1.0)
-          end
-        when "パー" then
-          if second.content == "グー" then
-            ActionCable.server.broadcast 'room_channel', message: @account1.email + " の勝ち"
-            @account1.update(win: @account1.win + 1.0)
-            @account2.update(lose: @account2.lose + 1.0)
-          elsif second.content == "チョキ" then
-            ActionCable.server.broadcast 'room_channel', message: @account2.email + " の勝ち"
-            @account2.update(win: @account2.win + 1.0)
-            @account1.update(lose: @account1.lose + 1.0)
-          elsif second.content == "パー" then
-            ActionCable.server.broadcast 'room_channel', message: "引き分けなのだ"
-            @account1.update(draw: @account1.draw + 1.0)
-            @account2.update(draw: @account2.draw + 1.0)
-          end
+        else
+          ActionCable.server.broadcast 'room_channel', message: "あなたはもう出しています..."
         end
-        @account1.update(rate: (@account1.win/(@account1.win+ @account1.lose + @account1.draw)).round(3))
-        @account2.update(rate: (@account2.win/(@account2.win+ @account2.lose + @account2.draw)).round(3))
-
-      else
-        ActionCable.server.broadcast 'room_channel', message: "相手の選択を待っています..."
       end
-
+    else
+      ActionCable.server.broadcast 'room_channel', message: "スタートを押してください"
     end
-
   end
 
   def timer(data)
-    ActionCable.server.broadcast 'room_channel', time: data['time']
+    if data["time"] > 0 then
+      ActionCable.server.broadcast 'room_channel', time: data['time']
+      @flug = 1
+    elsif data["time"] == 0 then
+      ActionCable.server.broadcast 'room_channel', time: "finished!"
+      @flug = 0
+      @lists = Message.where(created_at: 5  .second.ago..Time.now) #現時刻から二件
+      ActionCable.server.broadcast 'room_channel', message: @lists.count
+
+    end
   end
 
 end
